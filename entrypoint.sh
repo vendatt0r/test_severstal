b8c628c08137a2f34b249658b28b3a4252d7692d
vendatt0r <vasyok03020454@gmail.com>
2026-02-06 23:51:32 +0300

    feat: some fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-echo "Waiting for PostgreSQL..."
+set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+: "${POSTGRES_HOST:?Need to set POSTGRES_HOST}"
+: "${POSTGRES_PORT:?Need to set POSTGRES_PORT}"
+
+echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
+
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   sleep 1
 done
 
 echo "PostgreSQL is up - applying migrations"
 
 python manage.py makemigrations --noinput
-
 python manage.py migrate --noinput
 
-echo "Starting Django server"
-python manage.py runserver 0.0.0.0:8000
+echo "Starting Django server on 0.0.0.0:8000"
+exec python manage.py runserver 0.0.0.0:8000
